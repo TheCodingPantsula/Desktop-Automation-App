67e750da5993a02df975d83ada1b83d6c4010352
TheCodingPantsula <125494888+TheCodingPantsula@users.noreply.github.com>
2023-07-25 12:55:13 +0100

    refactored

--- automation.py
+++ automation.py
@@ -9,23 +9,36 @@ movies_path = 'C:/Users/smngv/Videos/Movies'
 music_path = 'C:/Users/smngv/Music'
 documents_path = 'C:/Users/smngv/Documents'
 pictures_path = 'C:/Users/smngv/OneDrive/Pictures/Downloaded'
+applications_path = 'C:/Users/smngv/OneDrive/Documents/Programs'
 
 # Create a dictionary to map file extensions to destination folders
 folders = {
     '.mkv': movies_path,
     '.mp4': movies_path,
+    '.srt': movies_path,
     '.mp3': music_path,
     '.exe': documents_path,
+    '.docx': documents_path,
+    '.webp': documents_path,
+    '.txt': documents_path,
     '.pdf': documents_path,
+    '.zip': documents_path,
+    '.PNG': pictures_path,
     '.png': pictures_path,
     '.jpeg': pictures_path,
-    '.jpg': pictures_path
+    '.jpg': pictures_path,
+    '.JPG': pictures_path,
+    '.avif': pictures_path,
+    '.ics': pictures_path,
+    '.msi': applications_path,
+    '.sql': applications_path,
+    '.svq': applications_path,
 }
 
 # Iterate over the files in the downloads folder
 for filename in os.listdir(downloads_path):
     # Get the file extension
-    file_ext = os.path.splitext(filename)[1]
+    file_ext = os.path.splitext(filename)[1].lower()
     
     # Check if the file extension is in the dictionary
     if file_ext in folders:
